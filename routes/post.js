fea2a92500c3f29157d87f702d753039ebbb4f4e
Sungjun <opr99@naver.com>
2020-10-07 12:04:57 +0900

    update

--- routes/post.js
+++ routes/post.js
@@ -1,19 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var approve = { 'admin1': 0, 'admin2': 0, 'admin3': 0 };
 
 router.post('/', function (req, res, next) {
   console.log('첫 번째 미들웨어 호출 됨');
-  var approve = { 'admin1': 0, 'admin2': 0, 'admin3': 0 };
 
   var paramId = req.body.item;
   console.log(`id : ${paramId}`);
 
   //아이디 일치여부 flag json 데이터입니다.
-  if (paramId == 'admin1') {
+  if (paramId === 'admin1') {
     approve.admin1 += 1;
     console.log(approve.admin1);
-  }
+  } else if (paramId === 'admin2') {
+    approve.admin2 += 1;
+    console.log(approve.admin2);
+  } else if (paramId === 'admin3') {
+    approve.admin3 += 1;
+    console.log(aprove.admin3);
+  } else console.log('일치하는 아이디가 없습니다!');
   res.send(approve);
 });
 
-module.exports = router;
+module.exports = router;  
