18d939cefb7e5bf69394e44941f4a3908ba19eec
Sungjun <opr99@naver.com>
2020-10-08 01:17:18 +0900

    update

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var app = express();
 app.set('view engine', 'ejs');
 
 console.log(count);
-var tit = 4;
+var tit = 3;
 router.get('/', function (req, res, next) {
   res.render('index', { title: tit });
 });
