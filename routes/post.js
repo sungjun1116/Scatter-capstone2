91a1c04e3124e44ae0a949d80e1d10673ae05201
Sungjun <opr99@naver.com>
2020-10-08 00:37:39 +0900

    Dfdf

--- routes/post.js
+++ routes/post.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var approve = { 'admin1': 0, 'admin2': 0, 'admin3': 0 };
+var count = { 'admin1': 0, 'admin2': 0, 'admin3': 0 };
 
 router.post('/', function (req, res, next) {
   var paramId = req.body.item;
@@ -8,18 +8,18 @@ router.post('/', function (req, res, next) {
 
   //아이디 일치여부 flag json 데이터입니다.
   if (paramId === 'admin1') {
-    approve.admin1 += 1;
-    console.log(approve.admin1);
+    count.admin1 += 1;
+    console.log(count.admin1);
   } else if (paramId === 'admin2') {
-    approve.admin2 += 1;
-    console.log(approve.admin2);
+    count.admin2 += 1;
+    console.log(count.admin2);
   } else if (paramId === 'admin3') {
-    approve.admin3 += 1;
-    console.log(aprove.admin3);
+    count.admin3 += 1;
+    console.log(count.admin3);
   } else console.log('일치하는 아이디가 없습니다!');
-  res.send(approve);
+  res.send(count);
 });
 
-global.count = approve;
+global.count = count;
 
 module.exports = router;
