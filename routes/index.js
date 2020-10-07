5c73a04270ce16bce155d6c09dae01f307733e0a
Sungjun <opr99@naver.com>
2020-10-08 00:24:10 +0900

    update

--- routes/index.js
+++ routes/index.js
@@ -1,34 +1,15 @@
 var express = require('express');
 var router = express.Router();
-var approve = { 'admin1': 0, 'admin2': 0, 'admin3': 0 };
+require('./post.js');
 var app = express();
 
 app.set('view engine', 'ejs');
 
-
-router.post('/index', function (req, res, next) {
-  var paramId = req.body.item;
-  console.log(`id : ${paramId}`);
-
-  //아이디 일치여부 flag json 데이터입니다.
-  if (paramId === 'admin1') {
-    approve.admin1 += 1;
-    console.log(approve.admin1);
-  } else if (paramId === 'admin2') {
-    approve.admin2 += 1;
-    console.log(approve.admin2);
-  } else if (paramId === 'admin3') {
-    approve.admin3 += 1;
-    console.log(aprove.admin3);
-  } else console.log('일치하는 아이디가 없습니다!');
-  res.send(approve);
-});
-var tit = approve.admin1;
+var tit = count.admin1;
 router.get('/', function (req, res, next) {
   res.render('index', { title: tit });
 });
 
 
-global.count = approve;
 
 module.exports = router;
