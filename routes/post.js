97ef9e4e226b964290724259a1764b1820a7b500
Sungjun <opr99@naver.com>
2020-10-08 23:00:30 +0900

    pm2 problem

--- routes/post.js
+++ routes/post.js
@@ -12,28 +12,22 @@ router.post('/', function (req, res, next) {
   console.log(`id : ${paramId}`);
   if (paramId === 'admin1') {
     approve.admin1 += 1;
-    console.log(approve.admin1);
     description = `var admin1 = approve.admin1;`;
-    console.log(description);
   } else if (paramId === 'admin2') {
     approve.admin2 += 1;
-    console.log(approve.admin2);
     description = `var admin2 = approve.admin2;`;
-    console.log(description);
   } else if (paramId === 'admin3') {
     approve.admin3 += 1;
-    console.log(aprove.admin3);
     description = `var admin3 = approve.admin3;`;
-    console.log(description);
   } else console.log('일치하는 아이디가 없습니다!');
-  console.log('뭐야이거');
 
-  // fs.writeFileSync(`../public/data/${getInfo}.js`, description, 'utf8', function (err) {
-  //   if (err === null) {
-  //     console.log('success');
-  //   }
-  //   else { console.log('fail'); }
-  // });
+
+  fs.writeFileSync(`../public/data/${getInfo}.js`, description, 'utf8', function (err) {
+    if (err === null) {
+      console.log('success');
+    }
+    else { console.log('fail'); }
+  });
   res.send(1);
 });
 
