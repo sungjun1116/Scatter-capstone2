baafd4ddd3549a7775b0d303de4b079eb5750e47
Sungjun <opr99@naver.com>
2020-10-08 14:36:43 +0900

    update

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,10 @@ require('./post.js');
 var app = express();
 
 app.set('view engine', 'ejs');
+// console.log(count);
 
-console.log('index로 잘 들어왔당께 :', count);
 router.get('/', function (req, res, next) {
-  var myVar = 3;
+  var myVar = count;
   res.render('index', { myVar: myVar });
 });
 
--- routes/post.js
+++ routes/post.js
@@ -1,28 +1,27 @@
 var express = require('express');
 var router = express.Router();
 var count = { 'admin1': 0, 'admin2': 0, 'admin3': 0 };
+let temp;
 
 router.post('/', function (req, res, next) {
   var paramId = req.body.item;
   console.log(`id : ${paramId}`);
-
   //아이디 일치여부 flag json 데이터입니다.
   if (paramId === 'admin1') {
     count.admin1 += 1;
-    console.log(count.admin1);
-    // global.count = count.admin1;
+    temp = count.admin1;
   } else if (paramId === 'admin2') {
     count.admin2 += 1;
-    console.log(count.admin2);
-    // global.count = count.admin2;
+    temp = count.admin2;
   } else if (paramId === 'admin3') {
     count.admin3 += 1;
-    console.log(count.admin3);
-    // global.count = count.admin3;
-  } else console.log('일치하는 아이디가 없습니다!');
+    temp = count.admin3;
+  } else {
+    temp = 1000;
+  }
   res.send(count);
 });
+console.log(count.admin1);
 
-global.count = count.admin1;
 
 module.exports = router;
