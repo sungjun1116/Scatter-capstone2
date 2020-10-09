ce6d3268f20e5298c606362acdfad0c24d33ace5
Sungjun <opr99@naver.com>
2020-10-09 11:49:51 +0900

    update

--- routes/post.js
+++ routes/post.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var fs = require('fs');
 
 
-var approve = { 'starbuks': 0, 'ediya': 0, 'azit': 0 };
+var cafe = { 'starbuks': 0, 'ediya': 0, 'azit': 0 };
 
 
 router.post('/', function (req, res, next) {
@@ -11,17 +11,17 @@ router.post('/', function (req, res, next) {
   var body;
   console.log(`id : ${paramId}`);
   if (paramId === 'starbuks') {
-    approve.starbuks += 1;
-    console.log(approve.starbuks);
-    body = `var starbuks = ${approve.starbuks};`;
+    cafe.starbuks += 1;
+    console.log(cafe.starbuks);
+    body = `var starbuks = ${cafe.starbuks};`;
   } else if (paramId === 'ediya') {
-    approve.ediya += 1;
-    console.log(approve.ediya);
-    body = `var ediya = ${approve.ediya};`;
+    cafe.ediya += 1;
+    console.log(cafe.ediya);
+    body = `var ediya = ${cafe.ediya};`;
   } else if (paramId === 'azit') {
-    approve.azit += 1;
-    console.log(approve.azit);
-    body = `var azit = ${approve.azit};`;
+    cafe.azit += 1;
+    console.log(cafe.azit);
+    body = `var azit = ${cafe.azit};`;
   } else console.log('일치하는 아이디가 없습니다!');
 
 
