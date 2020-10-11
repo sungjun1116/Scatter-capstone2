56241d439016c871dd6ab54517ade5809716c199
Sungjun <opr99@naver.com>
2020-10-11 15:37:14 +0900

    time

--- routes/post.js
+++ routes/post.js
@@ -9,21 +9,24 @@ let azit = 0;
 router.post('/', function (req, res, next) {
   var paramId = req.body.item;
   let body;
+  let target;
   console.log(`id : ${paramId}`);
   if (paramId === 'starbucks') {
-    starbucks += 1;
+    target = starbucks += 1;
     console.log(starbucks);
-    body = `var ${paramId} = ${starbucks}`;
+    // body = `var ${paramId} = ${starbucks}`;
   } else if (paramId === 'ediya') {
-    ediya += 1;
+    target = ediya += 1;
     console.log(ediya);
-    body = `var ${paramId} = ${ediya}`;
+    // body = `var ${paramId} = ${ediya}`;
   } else if (paramId === 'azit') {
-    azit += 1;
+    target = azit += 1;
     console.log(azit);
-    body = `var ${paramId} = ${azit}`;
+    // body = `var ${paramId} = ${azit}`;
   } else console.log('일치하는 아이디가 없습니다!');
 
+
+  body = `var ${paramId} = ${target}`;
   fs.writeFile(`./public/data/${paramId}.js`, body, 'utf8', function (err) {
     if (err === null) {
       console.log('success');
@@ -32,19 +35,16 @@ router.post('/', function (req, res, next) {
   });
 
   setTimeout(function () {
-    starbucks -= 1;
-    if (starbucks < 0) starbucks = 0;
-    ediya -= 1;
-    if (ediya < 0) ediya = 0;
-    azit -= 1;
-    if (azit < 0) azit = 0;
+    target -= 1;
+    body = `var ${paramId} = ${target}`;
     fs.writeFile(`./public/data/${paramId}.js`, body, 'utf8', function (err) {
       if (err === null) {
         console.log('success');
+        console.log();
       }
       else { throw err; }
     });
-  }, 5000)
+  }, 10000)
 
   res.redirect('/');
 });
