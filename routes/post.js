9caf69daa49c2e45be1ac58db94490e4893432ee
sungjun1116 <opr99@naver.com>
2021-04-10 20:00:59 +0900

    update

--- routes/post.js
+++ routes/post.js
@@ -1,13 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var fs = require('fs');
-let { time_cafe } = require('../lib/date');
+var fs = require("fs");
+let { time_cafe } = require("../lib/date");
 
 let starbucks = 0;
 let ediya = 0;
 let azit = 0;
 
-router.post('/', function (req, res, next) {
+router.post("/", function (req, res, next) {
   var paramId = req.body.item;
   var lat = req.body.lat;
   var lng = req.body.lng;
@@ -16,64 +16,66 @@ router.post('/', function (req, res, next) {
     console.log(`현재 위도: ${lat}`);
     console.log(`현재 경도: ${lng}`);
     let description = `var x = ${lat}; var y = ${lng}`;
-    fs.writeFile(`./public/data/gps.js`, description, 'utf8', function (err) {
+    fs.writeFile(`./public/data/gps.js`, description, "utf8", function (err) {
       if (err === null) {
-        console.log('Get GPS success');
+        console.log("Get GPS success");
+      } else {
+        throw err;
       }
-      else { throw err; }
     });
   }
   // writeFile 기능 구현
   else {
-    let body = '';
+    let body = "";
     console.log(`id : ${paramId}`);
-    if (paramId === 'starbucks') {
+    if (paramId === "starbucks") {
       starbucks += 1;
       console.log(starbucks);
       body = `var ${paramId} = ${starbucks}`;
-    } else if (paramId === 'ediya') {
+    } else if (paramId === "ediya") {
       ediya += 1;
       console.log(ediya);
       body = `var ${paramId} = ${ediya}`;
-    } else if (paramId === 'azit') {
+    } else if (paramId === "azit") {
       target = azit += 1;
       console.log(azit);
       body = `var ${paramId} = ${azit}`;
-    } else console.log('일치하는 아이디가 없습니다!');
+    } else console.log("일치하는 아이디가 없습니다!");
     if (body !== ``) {
-      fs.writeFile(`./public/data/cafe/${paramId}.js`, body, 'utf8', function (err) {
+      fs.writeFile(`./public/data/cafe/${paramId}.js`, body, "utf8", function (err) {
         if (err === null) {
-          console.log('Writefile success');
+          console.log("Writefile success");
+        } else {
+          throw err;
         }
-        else { throw err; }
       });
     }
     if (body !== ``) {
       setTimeout(function () {
-        if (paramId === 'starbucks') {
+        if (paramId === "starbucks") {
           starbucks -= 1;
           console.log(`남은 이용객 수 : ${starbucks}`);
           body = `var ${paramId} = ${starbucks}`;
-        } else if (paramId === 'ediya') {
+        } else if (paramId === "ediya") {
           ediya -= 1;
           console.log(`남은 이용객 수: ${ediya}`);
           body = `var ${paramId} = ${ediya}`;
-        } else if (paramId === 'azit') {
+        } else if (paramId === "azit") {
           azit -= 1;
           console.log(`남은 이용객 수: ${azit}`);
           body = `var ${paramId} = ${azit}`;
-        } else console.log('일치하는 값이 없습니다.');
-        fs.writeFile(`./public/data/cafe/${paramId}.js`, body, 'utf8', function (err) {
+        } else console.log("일치하는 값이 없습니다.");
+        fs.writeFile(`./public/data/cafe/${paramId}.js`, body, "utf8", function (err) {
           if (err === null) {
-            console.log('Update success');
+            console.log("Update success");
+          } else {
+            throw err;
           }
-          else { throw err; }
         });
-      }, time_cafe)
+      }, time_cafe);
     }
   }
-  res.redirect('/');
+  res.redirect("/");
 });
-
 
 module.exports = router;
