1553d3dce48bd1701c6e1fbbf2894bc47e13b88c
sungjun1116 <opr99@naver.com>
2021-04-10 20:43:04 +0900

    revert

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { myVar: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { myVar: "Express" });
+});
+
+router.post("/", function (req, res, next) {
+  const { lat, lng } = req.body;
+  if (lat !== undefined && lng !== undefined) {
+    console.log(`현재 위도: ${lat}`);
+    console.log(`현재 경도: ${lng}`);
+    let description = `var x = ${lat}; var y = ${lng}`;
+    fs.writeFile(`./public/data/gps.js`, description, "utf8", function (err) {
+      if (err === null) {
+        console.log("Get GPS success");
+      } else {
+        throw err;
+      }
+    });
+  } else {
+    let description = `var x = 37.322964; var y = 127.127272`;
+    fs.writeFile(`./public/data/gps.js`, description, "utf8", function (err) {
+      if (err === null) {
+        console.log("Get GPS success");
+      } else {
+        throw err;
+      }
+    });
+  }
 });
 
 module.exports = router;
