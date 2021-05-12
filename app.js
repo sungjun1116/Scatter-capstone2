0bd2242dc2a749ccd9d6789093b86a0e82f0b8ca
sungjun1116 <opr99@naver.com>
2021-05-12 11:40:11 +0900

    update location information

--- app.js
+++ app.js
@@ -1,37 +1,35 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var indexRouter = require('./routes/index');
-var gymRouter = require('./routes/gym');
-var restaurantRouter = require('./routes/restaurant');
-var newsRouter = require('./routes/news');
-var postRouter = require('./routes/post');
-var postRouter2 = require('./routes/post2');
-var postRouter3 = require('./routes/post3');
-var adminRouter = require('./routes/admin');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var indexRouter = require("./routes/index");
+var gymRouter = require("./routes/gym");
+var restaurantRouter = require("./routes/restaurant");
+var postRouter = require("./routes/post");
+var postRouter2 = require("./routes/post2");
+var postRouter3 = require("./routes/post3");
+var adminRouter = require("./routes/admin");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/post', postRouter);
-app.use('/post2', postRouter2);
-app.use('/post3', postRouter3);
-app.use('/gym', gymRouter);
-app.use('/restaurant', restaurantRouter);
-app.use('/news', newsRouter);
-app.use('/admin', adminRouter);
+app.use("/", indexRouter);
+app.use("/post", postRouter);
+app.use("/post2", postRouter2);
+app.use("/post3", postRouter3);
+app.use("/gym", gymRouter);
+app.use("/restaurant", restaurantRouter);
+app.use("/admin", adminRouter);
 
 // catch 404 and to error handler
 app.use(function (req, res, next) {
@@ -42,11 +40,11 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
